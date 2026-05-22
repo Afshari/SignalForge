d4f9ff95f3a72e694d3467bf21e277477e64d1c4
Mohsen Afshari <afshari.mvc@gmail.com>
2026-05-22 12:05:47 +0200

    feat: add cuFFT batch magnitude spectrum pipeline with unit tests

--- SignalForge_GPU/inc/gpu/SignalForge.cuh
+++ SignalForge_GPU/inc/gpu/SignalForge.cuh
@@ -6,3 +6,11 @@
 #include <stdio.h>
 #include <vector>
 #include "gpu/GPUHash.h"
+#include <cufft.h>
+
+
+__global__ void ComputeMagnitudes(
+    cufftComplex* d_freq,
+    float* d_magnitudes,
+    uint32_t      num_files,
+    uint32_t      fft_size);
